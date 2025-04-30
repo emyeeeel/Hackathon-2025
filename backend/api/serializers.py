a829ea2fa6a7de95dbbd7661c8326fd59f92e0e0
Emyeeeel <maryamielcatado@gmail.com>
2025-05-01 07:29:45 +0800

    added product model

---
+++ backend/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    total_weight = serializers.ReadOnlyField()  # Read-only field for the calculated property
+
+    class Meta:
+        model = Product
+        fields = [
+            'id', 'name', 'description', 'category', 'subcategory',
+            'width', 'height', 'length', 'weight', 'quantity',
+            'is_fragile', 'requires_refrigeration', 'expiration_date',
+            'created_at', 'updated_at', 'total_weight'
+        ]
