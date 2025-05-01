0f98dac5092940c9972e47c0740a279634727365
mfroche <marylle.roche.21@usjr.edu.ph>
2025-05-01 15:42:24 +0800

    Make migrations to Box and Packed box models

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,9 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework.viewsets import ModelViewSet
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Box, Product
+from .serializers import BoxSerializer, ProductSerializer
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class BoxViewSet(ModelViewSet):
+    queryset = Box.objects.all()
+    serializer_class = BoxSerializer
