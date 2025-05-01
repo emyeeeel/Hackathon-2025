5895f5780a1e1e68695568cc403537a02b577437
mfroche <marylle.roche.21@usjr.edu.ph>
2025-05-01 16:40:59 +0800

    Registered Product, Box, and Packed Box models to admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from backend.api.models import Box, PackedBox, Product
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Box)
+admin.site.register(PackedBox)
